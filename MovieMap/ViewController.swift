7c9811291f1cdacdbeab02b213f6afe1dd1cabc1
Wasla20 <63936700+Wasla20@users.noreply.github.com>
2023-10-04 10:47:04 +0500

    Initial Commit

---
+++ MovieMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMap
+//
+//  Created by Wasla Shafique on 04/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
